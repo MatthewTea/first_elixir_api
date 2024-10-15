bbe898753daf49d8f1d9ec597c3393e0467df022
Matthew Tea <matthewtea11@gmail.com>
2024-10-16 00:12:44 +0800

    add refresh endpoint to refresh JWT

--- lib/matthew_api_web/auth/error_response.ex
+++ lib/matthew_api_web/auth/error_response.ex
@@ -5,3 +5,7 @@ end
 defmodule MatthewApiWeb.Auth.ErrorResponse.Forbidden do
   defexception message: "You do not have the access to this resource.", plug_status: 403
 end
+
+defmodule MatthewApiWeb.Auth.ErrorResponse.NotFound do
+  defexception message: "Not Found", plug_status: 404
+end
--- lib/matthew_api_web/auth/guardian.ex
+++ lib/matthew_api_web/auth/guardian.ex
@@ -11,7 +11,7 @@ defmodule MatthewApiWeb.Auth.Guardian do
     {:error, :no_id_provided}
   end
 
-  def resources_from_claims(%{"sub" => id}) do
+  def resource_from_claims(%{"sub" => id}) do
     case Accounts.get_account!(id) do
       nil -> {:error, :not_found}
       resource -> {:ok, resource}
@@ -53,6 +53,12 @@ defmodule MatthewApiWeb.Auth.Guardian do
   def on_verify(claims, token, _options) do
     with {:ok, _} <- Guardian.DB.on_verify(claims, token) do
       {:ok, claims}
+    end
+  end
+
+  def on_refresh({old_token, old_claims}, {new_token, new_claims}, _options) do
+    with {:ok, _, _} <- Guardian.DB.on_refresh({old_token, old_claims}, {new_token, new_claims}) do
+      {:ok, {old_token, old_claims}, {new_token, new_claims}}
     end
   end
 
